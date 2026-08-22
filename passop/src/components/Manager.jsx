585c34c185557c24b9f99fbb9de60857dd289062
Yuvraj4v <ranayuvraj063@gmail.com>
2026-08-22 12:42:44 +0530

    Updated Frontend

--- passop/src/components/Manager.jsx
+++ passop/src/components/Manager.jsx
@@ -3,17 +3,20 @@ import React from 'react'
 const Manager = () => {
   return (
     <>
-    <div className=" bg-slate-50 max-w-4xl mycontainer">
-      <h1>PassOP</h1>
-      <p>Your own Password Manager</p>
-    <div className='text-white flex flex-col p-4'>
-      <input className='rounded-full' type="text" name='' id='' />
-      <div className='flex'>
-        <input type="text" />
-        <input type="text" />
+      <div className=" bg-slate-50 max-w-4xl mycontainer">
+        <h1 className='text-4xl text font-bold text-center'><span className='text-green-700'> &lt;</span>
+          <span>Pass</span><span className='text-green-700'>OP/&gt;</span></h1>
+        <p className='text-green-900 text-lg text-center'>Your own Password Manager</p>
+
+        <div className='text-black flex flex-col p-4 gap-8'>
+          <input className='rounded-full border border-green-500 w-full text-black p-4 py-1' type="text" name='' id='' />
+          <div className='flex w-full justify-between gap-8'>
+            <input className='rounded-full border border-green-500 w-full text-black p-4 py-1' type="text" name='' id='' />
+            <input className='rounded-full border border-green-500 w-full text-black p-4 py-1' type="text" name='' id='' />
+          </div>
+          <button>Add Password</button>
+        </div>
       </div>
-    </div>
-    </div>
     </>
   )
 }
