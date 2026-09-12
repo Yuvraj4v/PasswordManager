1982df5db7b4c9d59d1c6afe7d5cb26c309eec28
Yuvraj4v <ranayuvraj063@gmail.com>
2026-09-12 16:13:22 +0530

    updated

--- passop-mongo/Backend/server.js
+++ passop-mongo/Backend/server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv')
 const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
 
 dotenv.config()
 
+const app = express()
+const port = 3000
+app.use(bodyParser.json())
+
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = process.env.MONGO_URI;
 const client = new MongoClient(url);
 
 // Database Name
 const dbName = 'passop';
-const app = express()
-const port = 3000
 
-client.connect();
+const db = client.db(dbName);
 
 app.get('/', async (req, res) => {
+    const collection = db.collection('documents');
+    const findResult = await collection.find({}).toArray();
+    res.json(findResult)
+})
+
+app.post('/', async (req, res) => {
     const collection = db.collection('documents');
     const findResult = await collection.find({}).toArray();
     res.json(findResult)
